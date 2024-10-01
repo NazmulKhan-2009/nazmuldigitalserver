cc3be5bd18c30c2213a0b4cf94d3ef3663e29b62
A B M Nazmul Hossain Khan <nazmulustc09@gmail.com>
2024-10-01 17:45:54 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const uri=`mongodb+srv://nkadmin:nazifa1309@cluster0.qnbwm.mongodb.net/nazmulinmarket?retryWrites=true&w=majority`
+const uri=`mongodb+srv://nkadmin:nazifa---9@cluster0.qnbwm.mongodb.net/nazmulinmarket?retryWrites=true&w=majority`
 
 const app = express()
 
